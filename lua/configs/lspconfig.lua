cf369adab9c8208de90a9e80d2495d9e8043bc07
Edouard Berthe <edouard.berthe@protonmail.com>
2025-01-18 01:11:10 +0000

    Update deps

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -16,6 +16,12 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+}
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
