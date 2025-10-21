20b578b3b743fbb111bd7216f9b44b2c57473782
Edouard Berthe <edouard.berthe@protonmail.com>
2025-10-21 12:19:00 +0100

    Add `autocmds.lua` file to write auto commands + add command to open neo-tree by default

---
+++ lua/autocmds.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("NeotreeOnOpen", { clear = true }),
+  once = true,
+  callback = function(_)
+    if vim.fn.argc() == 0 then
+      vim.cmd "Neotree"
+    end
+  end,
+})
